b91550b5a9a87d4b8b12e08215dcda7225f52103
cesar-gz <cesargutierrezceja@gmail.com>
2022-10-01 13:43:34 -0700

    Made the file read from the source testing code instead of the input for testing.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import Lexical_Analyzer
 
+# Uncomment the block below if you want to go back to testing through command line.
+# Then ident the rest of the code by one Tab Space because of Python's scope rules.
+# Lastly comment out lines 13-16
+"""
 while True:
     text = input('Suffering > ')
     answer, oopsie = Lexical_Analyzer.run('<stdin>', text)
+"""
 
-    if oopsie: print(oopsie.convert_string())
-    else: 
-        # print a token each row
-        for x in range(len(answer)):
-            print(answer[x])
+# change paramater of testFile.txt to whatever input file you want to test
+file = open("testFile.txt", "r")
+inputText = file.read()
+file.close()
+answer, oopsie = Lexical_Analyzer.run('<stdin>', inputText)
+
+if oopsie: print(oopsie.convert_string())
+else: 
+    # print a token each row
+    for x in range(len(answer)):
+        print(answer[x])
