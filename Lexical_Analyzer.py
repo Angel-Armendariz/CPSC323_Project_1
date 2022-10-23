c8b5a92293698243bcdb83091408854502482b46
Angel Armendariz <angela126509@gmail.com>
2022-10-23 15:25:23 -0700

    fixed some errors in the python naming

--- Lexical_Analyzer.py
+++ Lexical_Analyzer.py
@@ -371,15 +371,15 @@ class Parser:
 			if res.error: return res
 			return res.success(UnaryOpNode(tok, factor))
 		
-		elif tok.type in (TOK_INT, TOK_FLOAT):
+		elif tok.type in (TOK_INT, TOK_REAL):
 			res.register(self.advance())
 			return res.success(NumberNode(tok))
 
-		elif tok.type == TT_LPAREN:
+		elif tok.type == TOK_LPAREN:
 			res.register(self.advance())
 			expr = res.register(self.expr())
 			if res.error: return res
-			if self.current_tok.type == TT_RPAREN:
+			if self.current_tok.type == TOK_RPAREN:
 				res.register(self.advance())
 				return res.success(expr)
 			else:
@@ -394,10 +394,10 @@ class Parser:
 		))
 
 	def term(self):
-		return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+		return self.bin_op(self.factor, (TOK_MUL, TOK_DIV))
 
 	def expr(self):
-		return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+		return self.bin_op(self.term, (TOK_PLUS, TOK_MINUS))
 
 	###################################
 
