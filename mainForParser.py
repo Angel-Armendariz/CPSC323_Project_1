d8f80b74d2d480c295e983ab9324d455a20bac1a
cesar-gz <cesargutierrezceja@gmail.com>
2022-10-23 15:14:00 -0700

    progress.

---
+++ mainForParser.py
@@ -0,0 +1,22 @@
+import Syntax_Analyzer
+
+parserFile = open("testCase1.txt", "r")
+inputText = parserFile.read()
+parserFile.close()
+answer, oopsie = Syntax_Analyzer.run('<stdin>', inputText)
+
+f = open("outputFile1.txt", "w")
+for item in answer:
+    f.write("%s\n" % item)
+f.close()
+
+if oopsie: 
+    print(oopsie.convert_string())
+    f = open("outputFile.txt", "w")
+    f.write(oopsie.convert_string())
+    f.close()
+
+else: 
+    # print a token each row
+    for x in range(len(answer)):
+        print(answer[x])
