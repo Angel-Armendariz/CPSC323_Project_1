2314f42e2980b22acb186f7b4b5a4d03e6e23c9c
Angel Armendariz <angela126509@gmail.com>
2022-12-09 20:34:47 -0800

    Revert "Merge branch 'cesarOnly' of https://github.com/Angel-Armendariz/CPSC323_Project_1 into cesarOnly"
    This reverts commit 211d63b6165a9fa8783260151cd708fd95b7408b, reversing
    changes made to 80cd4fb02d1e086a9939fb4e6fbf3afdd7bdaebb.

--- main.py
+++ main.py
@@ -15,12 +15,32 @@ file.close()
 answer, oopsie = Lexical_Analyzer.run('<stdin>', inputText)
 
 # Prints out the test file tokens out to outputFile.txt which gets overwritten each time 
-f = open("outputFile1.txt", "w")
+f = open("outputFile.txt", "w")
 for item in answer:
     f.write("%s\n" % item)
 f.close()
 
-lexerFile = open("outputFile1.txt", "r")
+"""
+file = open("testCase2.txt", "r")
+inputText = file.read()
+file.close()
+answer, oopsie = Lexical_Analyzer.run('<stdin>', inputText)
+f = open("outputFile2.txt", "w")
+for item in answer:
+    f.write("%s\n" % item)
+f.close()
+
+file = open("testCase3.txt", "r")
+inputText = file.read()
+file.close()
+answer, oopsie = Lexical_Analyzer.run('<stdin>', inputText)
+f = open("outputFile3.txt", "w")
+for item in answer:
+    f.write("%s\n" % item)
+f.close()
+"""
+
+lexerFile = open("outputFile.txt", "r")
 parseFile = lexerFile.read()
 lexerFile.close()
 newFile = Syntax_Analyzer.parse(parseFile)
@@ -29,3 +49,16 @@ f = open("parsedFile.txt", "w")
 for item in newFile:
     f.write("%s\n" % item)
 f.close()
+
+"""
+if oopsie: 
+    print(oopsie.convert_string())
+    f = open("outputFile.txt", "w")
+    f.write(oopsie.convert_string()
+    f.close()
+
+else: 
+    # print a token each row
+    for x in range(len(answer)):
+        print(answer[x])
+"""
